e84a0645f60a8eb251453e4b697133d809c214a4
JoelCarr619 <JoelCarr619@gmail.com>
2023-03-30 09:45:20 -0700

    story 4 completed

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -7,8 +7,17 @@ class BlogController < ApplicationController
      end
      def new
        @blog = Blog.new 
-    
+     end
+     def create
+        @blog = Blog.create(blog_params)
+        if @blog.valid?
+            redirect_to blogs_path  
+        end
      end
 
+     private
+     def blog_params
+        params.require(:blog).permit(:title, :content)
+     end
 
 end
